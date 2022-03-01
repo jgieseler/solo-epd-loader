97dce985a91ed306fb71e4078a941839e114cda0
jgieseler <jan.gieseler@utu.fi>
2022-03-01 18:07:22 +0200

    change versioning to versioneer

--- solo_epd_loader/__init__.py
+++ solo_epd_loader/__init__.py
@@ -1,11 +1,3 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass  # package is not installed
-
 import datetime as dt
 import glob
 import itertools
@@ -18,6 +10,10 @@ import urllib.request
 from pathlib import Path
 from astropy.io.votable import parse_single_table
 import cdflib
+
+
+from . import version
+__version__ = version.get_versions()['version']
 
 
 """
